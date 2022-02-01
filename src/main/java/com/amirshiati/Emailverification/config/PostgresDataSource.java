80d00875b424f8fa54270e4e677454dec4341a60
amir shiati <amirshiati98@gmail.com>
2022-02-01 14:04:57 +0330

    Added database configs

---
+++ src/main/java/com/amirshiati/Emailverification/config/PostgresDataSource.java
@@ -0,0 +1,19 @@
+package com.amirshiati.Emailverification.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresDataSource {
+    @Bean
+    @ConfigurationProperties("datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
