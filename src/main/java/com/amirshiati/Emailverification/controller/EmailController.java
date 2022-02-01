3a7156b236d63b1322d01e98b26088b509a31ec7
amir shiati <amirshiati98@gmail.com>
2022-02-01 14:45:44 +0330

    Added send code route

--- src/main/java/com/amirshiati/Emailverification/controller/EmailController.java
+++ src/main/java/com/amirshiati/Emailverification/controller/EmailController.java
@@ -3,14 +3,16 @@ package com.amirshiati.Emailverification.controller;
 import com.amirshiati.Emailverification.entity.EmailModel;
 import com.amirshiati.Emailverification.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@Validated
 public class EmailController {
     private final EmailService service;
 
@@ -22,5 +24,10 @@ public class EmailController {
     @GetMapping("/emails")
     public List<EmailModel> getAll() {
         return service.getAll();
+    }
+
+    @PostMapping("/send")
+    public EmailModel sendCode(@RequestParam("email") @Email(message = "Invalid email format!") @NotBlank(message = "Email can't be empty!") String email) throws Exception {
+        return service.sendCode(email);
     }
 }
--- src/main/java/com/amirshiati/Emailverification/service/EmailService.java
+++ src/main/java/com/amirshiati/Emailverification/service/EmailService.java
@@ -1,23 +1,52 @@
 package com.amirshiati.Emailverification.service;
 
+import com.amirshiati.Emailverification.config.CodeConfig;
 import com.amirshiati.Emailverification.entity.EmailModel;
+import com.amirshiati.Emailverification.helper.CodeGeneratorHelper;
+import com.amirshiati.Emailverification.helper.JavaMailSenderHelper;
 import com.amirshiati.Emailverification.repository.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class EmailService {
 
     private final EmailRepository repository;
+    private final CodeGeneratorHelper codeGeneratorHelper;
+    private final JavaMailSenderHelper mailSenderHelper;
+    private final CodeConfig codeConfig;
+
 
     @Autowired
-    public EmailService(EmailRepository repository) {
+    public EmailService(EmailRepository repository, CodeGeneratorHelper codeGeneratorHelper, JavaMailSenderHelper mailSenderHelper, CodeConfig codeConfig) {
         this.repository = repository;
+        this.codeGeneratorHelper = codeGeneratorHelper;
+        this.mailSenderHelper = mailSenderHelper;
+        this.codeConfig = codeConfig;
     }
 
     public List<EmailModel> getAll() {
         return repository.findAll();
+    }
+
+    public EmailModel sendCode(String email) throws Exception {
+        String code = codeGeneratorHelper.getCode();
+        try {
+            mailSenderHelper.sendEmailWithAttachment(email, "Test", code);
+            LocalDateTime time = LocalDateTime.now();
+            EmailModel emailModel = new EmailModel();
+            emailModel.setAddress(email);
+            emailModel.setCode(code);
+            emailModel.setSendTime(time);
+            emailModel.setValidTime(time.plusMinutes(codeConfig.getValidForMinutes()));
+            return repository.save(emailModel);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new Exception();
+        }
     }
 }
