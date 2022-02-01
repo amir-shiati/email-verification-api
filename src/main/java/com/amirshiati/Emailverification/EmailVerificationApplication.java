1a703e80c639fd5a0b63c2e0b0a7dad8aac10750
amir shiati <amirshiati98@gmail.com>
2022-02-01 11:46:58 +0330

    Initial commit

---
+++ src/main/java/com/amirshiati/Emailverification/EmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.amirshiati.Emailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amirshiati/Emailverification/EmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirshiati.Emailverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
